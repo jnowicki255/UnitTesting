cd0f655b5f039aae80f4db7cae8945956fc99d28
Jacek Nowicki <jacek.nowicki@acciosoftware.pl>
2022-04-23 09:27:35 +0200

    Add project files.

---
+++ UnitTesting.Tests/ArithmeticsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting.Tests
+{
+    public class ArithmeticsTests
+    {
+        [Theory]
+        [InlineData(5.0, 3.0)]
+        [InlineData(2.5, 1.2)]
+        [InlineData(2, 2)]
+        [InlineData(double.MaxValue, double.MaxValue)]
+        public void Add_ValidValues_ValidResult(double a, double b)
+        {
+            // Arrange
+            var expected = a + b;
+
+            // Act
+            var result = Arithmetics.Add(a, b);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        public void Divide_InvalidValues_ExceptionThrown()
+        {
+
+        }
+    }
+}
---
+++ UnitTesting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5;
+            double b = 4.6;
+
+            Console.WriteLine(Math.Round(Arithmetics.Add(a, b), 2));
+            Console.WriteLine(Math.Round(Arithmetics.Substract(a, b), 2));
+            Console.WriteLine(Math.Round(Arithmetics.Multiply(a, b), 2));
+            Console.WriteLine(Math.Round(Arithmetics.Divide(a, b), 2));
+        }
+    }
+}
